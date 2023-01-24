014d4eff5e0bd9d24d231ce81c4f88a53a2657f1
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-01-24 12:40:41 -0800

    add easier reminders

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,18 @@ import 'module-alias/register'
 
 import * as cron from 'node-cron'
 import client from '@/helpers/client'
+import reminders from '@/data/reminders'
 
 // Straighten the back
-cron.schedule('0 * * * *', async () => {
-  try {
-    await client.publishCast('Watch your posture! Straighten your back!')
-    console.log('Cast published')
-  } catch (error) {
-    console.error(error instanceof Error ? error.message : error)
-  }
-})
+for (const [cronTime, text] of reminders) {
+  cron.schedule(cronTime, async () => {
+    try {
+      await client.publishCast(text)
+      console.log(cronTime, text)
+    } catch (error) {
+      console.error(error instanceof Error ? error.message : error)
+    }
+  })
+  console.log('Scheduled:', cronTime, text)
+}
 console.log('App started!')
---
+++ src/data/reminders.ts
@@ -0,0 +1,4 @@
+export default [['0 * * * *', 'Watch your posture! Straighten your back!']] as [
+  string,
+  string
+][]
